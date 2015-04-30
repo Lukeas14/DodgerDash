4155c6804ac55caf216105b78cf85252892e3b97
Justin Lucas <Lukeas14@gmail.com>
2015-04-30 11:07:01 -0700

    Added getPlayersByTeam

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ cacheManager.initCache();
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'application/json'});
-	console.log(cache.teams[119]);
 	res.end(JSON.stringify(cache.teams[119]));
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
