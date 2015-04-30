51bef1390d683bf2805255982a5724b89d798fe1
Justin Lucas <Lukeas14@gmail.com>
2015-04-30 00:38:24 -0700

    Now caching team information

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var http = require('http'),
 	util = require('util'),
 	config = require('./lib/config.json'),
 	cacheManager = require('./lib/cacheManager');
+	cache = require('./lib/cache');
 
-cacheManager.init();
+cacheManager.initCache();
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	console.log(cache.teams[119]);
+	res.end(JSON.stringify(cache.teams[119]));
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
