cf8da8cef31949b0d2fc4fa0cf18b46060d8dbf0
Justin Lucas <Lukeas14@gmail.com>
2015-05-03 13:24:30 -0700

    Installed expressJS

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var http = require('http'),
 	util = require('util'),
+	express = require('express'),
 	config = require('./lib/config.json'),
 	cacheManager = require('./lib/cacheManager');
 	cache = require('./lib/cache');
 
 cacheManager.initCache();
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	res.end(JSON.stringify(cache.teams[119]));
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var app = express();
+
+app.get('/test', function(req, res){
+	res.send(JSON.stringify(cache.teams[119]));
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(1337, function(){
+	var host = server.address().address,
+		port = server.address().port;
+
+	console.log('Server running on port %s.', port);
+});
