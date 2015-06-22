e7a392d629a114f6cb18ad7c0b9f27706b15ff82
Lucas, Justin <Justin.Lucas@mlb.com>
2015-06-22 16:05:08 -0700

    Using Bunyan for logging. No longer saving entire linescore and boxscore to cache for old games.

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var http = require('http'),
 	util = require('util'),
 	express = require('express'),
 	config = require('./lib/config.json'),
-	cacheManager = require('./lib/cacheManager');
-	cache = require('./lib/cache');
+	cacheManager = require('./lib/cacheManager'),
+	cache = require('./lib/cache'),
+	log = require('./lib/log');
 
 cacheManager.initCache();
 setInterval(function() {
@@ -17,11 +18,15 @@ app.get('/getTeam/*', function(req, res){
 	res.json(cache.teams[teamId]);
 });
 
+app.get("/cache", function(req, res){
+	res.json(cache);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(1337, function(){
 	var host = server.address().address,
 		port = server.address().port;
 
-	console.log('Server running on port %s.', port);
+	log.info("Server running on port %s", port);
 });
